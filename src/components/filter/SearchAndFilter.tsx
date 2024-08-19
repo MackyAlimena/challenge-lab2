e9c857c72418817f39495c57ee43cb138b54238c
MackyAlimena <128090305+MackyAlimena@users.noreply.github.com>
2024-08-19 02:48:05 -0300

    searching ok

--- src/components/filter/SearchAndFilter.tsx
+++ src/components/filter/SearchAndFilter.tsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { fetchFilteredSpotifyItems } from '../../api/ApiFilter';
 import { SpotifyItem } from '../../types/spotifyTypes'; // Adjust the path as needed
+import './SearchAndFilter.css';
 
 interface SearchAndFilterProps {
     onSearch: (items: SpotifyItem[]) => void;
@@ -10,11 +11,13 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({ onSearch }) => {
     const [query, setQuery] = useState<string>('');
     const [genre, setGenre] = useState<string>('');
     const [releaseDate, setReleaseDate] = useState<string>('');
+    const [isPopupVisible, setIsPopupVisible] = useState<boolean>(false);
 
     const handleSearch = async () => {
         try {
-            const items = await fetchFilteredSpotifyItems(query, genre, releaseDate);
+            const items = await fetchFilteredSpotifyItems( query, genre, releaseDate );
             onSearch(items);
+            setIsPopupVisible(false);
         } catch (error) {
             console.error('Error searching:', error);
         }
@@ -22,25 +25,35 @@ const SearchAndFilter: React.FC<SearchAndFilterProps> = ({ onSearch }) => {
 
     return (
         <div>
-            <input
-                type="text"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-                placeholder="Buscar..."
-            />
-            <input
-                type="text"
-                value={genre}
-                onChange={(e) => setGenre(e.target.value)}
-                placeholder="Género..."
-            />
-            <input
-                type="text"
-                value={releaseDate}
-                onChange={(e) => setReleaseDate(e.target.value)}
-                placeholder="Fecha de lanzamiento..."
-            />
-            <button onClick={handleSearch}>Buscar</button>
+            <button onClick={() => setIsPopupVisible(true)}>Buscar</button>
+            {isPopupVisible && (
+                <div className="popup-container">
+                    <div className="popup-content">
+                        <button className="close-button" onClick={() => setIsPopupVisible(false)}>X</button>
+                        <div className="search-filter-container">
+                            <input
+                                type="text"
+                                value={query}
+                                onChange={(e) => setQuery(e.target.value)}
+                                placeholder="Buscar..."
+                            />
+                            <input
+                                type="text"
+                                value={genre}
+                                onChange={(e) => setGenre(e.target.value)}
+                                placeholder="Género..."
+                            />
+                            <input
+                                type="text"
+                                value={releaseDate}
+                                onChange={(e) => setReleaseDate(e.target.value)}
+                                placeholder="Fecha de lanzamiento..."
+                            />
+                            <button onClick={handleSearch}>Buscar</button>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
     );
 };
